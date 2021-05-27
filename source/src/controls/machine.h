b330eaedcf8198ae3850317f7a2310bc4e875b09
Gerard Sequeira <gsequeira@boston-engineering.com>
2021-05-27 16:22:47 -0400

    State machine framework.

---
+++ source/src/controls/machine.h
@@ -0,0 +1,46 @@
+#ifndef UVENT_MACHINE_H
+#define UVENT_MACHINE_H
+
+void machine_run();
+void machine_setup();
+
+enum class States
+{
+    ST_STARTUP,
+    ST_INSPR,
+    ST_INSPR_HOLD,
+    ST_EXPR,
+    ST_PEEP_PAUSE,
+    ST_EXPR_HOLD,
+    ST_PADLE_HOME,
+    ST_OFF
+};
+
+class Machine
+{
+    public:
+        void setup();
+        void run();
+        const char* get_current_state_string();
+        States get_current_state();
+    private:
+        // Default startup state
+        States state;
+
+        // Condition to evaluate code on first entry into a state
+        bool state_first_entry = false;
+
+        // Set the current state in the state machine
+        void set_state(States);
+
+        // State functions
+        void state_startup();
+        void state_inspiration();
+        void state_inspiration_hold();
+        void state_expiration();
+        void state_peep_pause();
+        void state_expiration_hold();
+        void state_paddle_home();
+        void state_off();
+};
+#endif
