6a6187a1effa47d12831d880a206e22d2d5a472d
Gerard Sequeira <gsequeira@boston-engineering.com>
2021-06-25 07:32:49 -0400

    Add storage layer with read and write settings api. If settings is written to the eeprom, the CRC is also written to a separate location.

---
+++ source/src/eeprom/storage.h
@@ -0,0 +1,45 @@
+#ifndef UVENT_STORAGE_H
+#define UVENT_STORAGE_H
+
+#include "../config/uvent_conf.h"
+#include "SparkFun_External_EEPROM.h"
+
+// UVent settings structure
+struct __attribute__((packed)) uvent_settings {
+    /* Julian Date: YYMMDD
+     * Lot:         L: A,B,C...
+     * Serial:      XXXX
+     */
+    char serial[12];            // YYMMDDLXXXX
+    uint16_t diff_pressure_type;// 0: ±0.07, 1: ±0.09
+    uint16_t actuator_home_offset_adc_counts;
+    int16_t gpressure_offset_adc_counts;
+    int16_t dpressure_offset_adc_counts;
+};
+
+class Storage {
+public:
+    bool init();
+    bool is_crc_ok();
+    void load_defaults();
+    void get_settings(uvent_settings&);
+    void set_settings(uvent_settings&);
+#if DEBUG_EEPROM
+    void display_storage();
+#endif
+
+private:
+    ExternalEEPROM external_eeprom;
+
+    // Defaults cal. settings
+    uvent_settings def_settings = {
+            .serial = "210000A0000",
+            .diff_pressure_type = 0,
+            .actuator_home_offset_adc_counts = 0,
+            .gpressure_offset_adc_counts = 0,
+            .dpressure_offset_adc_counts = 0};
+
+    uint32_t crc_calculate();
+};
+
+#endif
