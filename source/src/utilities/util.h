09bb541956165adb02d00c8ebd96e3efaa692184
Gerard Sequeira <gsequeira@boston-engineering.com>
2021-07-07 14:53:46 -0400

    Get current time(s) since startup

--- source/src/utilities/util.h
+++ source/src/utilities/util.h
@@ -10,4 +10,7 @@
  */
 bool has_time_elapsed(uint32_t* ptr, uint32_t n);
 
-#endif //UVENT_UTIL_H
+// Returns the current time in seconds
+inline float now_s() { return millis() * 1e-3; }
+
+#endif//UVENT_UTIL_H
