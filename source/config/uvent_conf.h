10bfcd8d48070dd53554e71c244c91bf0a1ce605
Gerard Sequeira <gsequeira@boston-engineering.com>
2021-07-28 16:53:56 -0400

    Increment version to 0.0.2

--- source/config/uvent_conf.h
+++ source/config/uvent_conf.h
@@ -9,7 +9,7 @@
 // Version numbers
 #define UVENT_VERSION_MAJOR 0
 #define UVENT_VERSION_MINOR 0
-#define UVENT_VERSION_PATCH 1
+#define UVENT_VERSION_PATCH 2
 
 // Screen Dimensions
 #define SCREEN_WIDTH 800
